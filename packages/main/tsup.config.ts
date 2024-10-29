68d6fac6b0a6623f637c9e36528c0b7f32e4447e
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-10-29 09:25:16 +0530

    refactor: minify build

--- packages/main/tsup.config.ts
+++ packages/main/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig((options: Options) => ({
   entry: ['./src/index.ts'],
   clean: true,
   dts: true,
+  minify: true,
   format: ['esm', 'cjs', 'iife'],
   tsconfig: './tsconfig.json',
   ...options,
