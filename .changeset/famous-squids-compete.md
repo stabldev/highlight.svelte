8484f073ee8886600698dff2c81ebdb902a1ac97
ilovemoon <moonlitgrace.gaia@gmail.com>
2024-11-04 20:08:14 +0530

    feat: Svelte 5 runes syntax support (#38)

---
+++ .changeset/famous-squids-compete.md
@@ -0,0 +1,5 @@
+---
+'highlight.svelte': patch
+---
+
+feat: Svelte 5 runes syntax support
--- packages/main/src/svelte.js
+++ packages/main/src/svelte.js
@@ -1,30 +1,68 @@
 /*
 Language: Svelte.js
 Requires: xml.js, javascript.js, css.js, typescript.js (optional)
-Author: Alexey Schebelev
-Description: Components of Svelte Framework
+Inspiration from: Alexey Schebelev
 Link: https://github.com/AlexxNB/highlightjs-svelte/blob/master/src/svelte.js
 */
 
-export default function (hljs) {
+export default function(hljs) {
+  const common_patterns = [
+    {
+      begin: /\$([a-zA-Z_][\w-]*)\s*\(/,
+      className: 'title function_',
+      endsWithParent: true,
+      variants: [
+        {
+          begin: /\$([a-zA-Z_][\w-]*)\s*\(/,
+          end: /\(/,
+          returnBegin: true,
+          excludeEnd: true,
+          relevance: 10,
+        },
+      ],
+    },
+    {
+      begin: /\$effect\.(pre|tracking|root)/,
+      className: 'title function_',
+      endsWithParent: true,
+      variants: [
+        {
+          begin: /\$effect\.(pre|tracking|root)/,
+          end: /(?=\s*\()/,
+          returnBegin: true,
+          excludeEnd: true,
+          relevance: 10,
+        },
+      ],
+    },
+    {
+      begin: /[#@](if|each|key|await|snippet|render|html|const|debug)/,
+      className: 'keyword',
+      relevance: 10,
+    },
+    {
+      begin: /\b(bind|use|transition|in|out|animate|class|style):/gm,
+      className: 'variable',
+      relevance: 10,
+    },
+    {
+      begin: /^\s*(let|bind)\s+[a-zA-Z_][\w-]*/,
+      className: 'variable',
+      relevance: 10,
+    },
+  ];
+
   return {
     subLanguage: 'xml',
     contains: [
-      hljs.COMMENT('<!--', '-->', {
-        relevance: 10,
-      }),
+      hljs.COMMENT('<!--', '-->', { relevance: 10 }),
       {
         begin: /<script(?!.*lang=["']ts["'])>/gm,
         end: /<\/script>/gm,
         subLanguage: 'javascript',
         excludeBegin: true,
         excludeEnd: true,
-        contains: [
-          {
-            begin: /^\s*\$:/gm,
-            className: 'keyword',
-          },
-        ],
+        contains: common_patterns,
       },
       {
         begin: /<script\s+lang=["']ts["']>/gm,
@@ -32,12 +70,7 @@ export default function (hljs) {
         subLanguage: 'typescript',
         excludeBegin: true,
         excludeEnd: true,
-        contains: [
-          {
-            begin: /^\s*\$:/gm,
-            className: 'keyword',
-          },
-        ],
+        contains: common_patterns,
       },
       {
         begin: /^(\s*)(<style.*>)/gm,
@@ -47,19 +80,21 @@ export default function (hljs) {
         excludeEnd: true,
       },
       {
-        begin: /\{/gm,
-        end: /\}/gm,
+        begin: /\{/,
+        end: /\}/,
         subLanguage: 'javascript',
         contains: [
+          hljs.COMMENT(/\/\*/, /\*\//),
+          { begin: /\{/, end: /\}/, skip: true },
           {
-            begin: /\{/,
-            end: /\}/,
-            skip: true,
-          },
-          {
-            begin: /([#:\/@])(if|else|each|await|then|catch|debug|html)/gm,
+            begin: /[\s:#@$%&*+\-|^~!=<>,./?;\s]+/,
             className: 'keyword',
             relevance: 10,
+          },
+          {
+            begin: /[a-zA-Z_][\w-]*/,
+            className: 'variable',
+            relevance: 0,
           },
         ],
       },
